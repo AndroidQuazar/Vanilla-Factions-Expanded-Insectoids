bc1881df7c0034c60abd67ab5a7ae3da750911d4
Kylian <kylian.balde@gmail.com>
2020-06-09 15:21:09 +0200

    Potential infinite loop fix

--- 1.1/Source/VFEI/Events/IncidentWorker_FirstRaid.cs
+++ 1.1/Source/VFEI/Events/IncidentWorker_FirstRaid.cs
@@ -18,19 +18,20 @@ namespace VFEI.Events
 
 		protected override bool TryExecuteWorker(IncidentParms parms)
 		{
+			PlayerKnowledgeDatabase.SetKnowledge(ConceptDefOf.ShieldBelts, 1f);
 			Map map1 = (Map)parms.target;
 			IncidentParms incidentParms = StorytellerUtility.DefaultParmsNow(IncidentCategoryDefOf.ThreatBig, map1);
 			incidentParms.faction = Find.FactionManager.AllFactionsVisible.Where((f) => f.def.defName == "VFEI_Insect").First();
 			incidentParms.raidStrategy = RaidStrategyDefOf.ImmediateAttack;
 			incidentParms.raidArrivalMode = PawnsArrivalModeDefOf.EdgeWalkIn;
 			incidentParms.points = incidentParms.points * 2;
-			Log.Message("Faction: " + incidentParms.faction.ToString());
-			Log.Message("RaidStrategy: " + incidentParms.raidStrategy.ToString());
-			Log.Message("RaidArrivalMode: " + incidentParms.raidArrivalMode.ToString());
-			Log.Message("Points: " + incidentParms.points.ToString());
-			Log.Message("PawnCount: " + incidentParms.pawnCount.ToString());
-			Log.Message("SpawnCenter: " + incidentParms.spawnCenter.ToString());
-			Log.Message("Target: " + incidentParms.target.ToString());
+			//Log.Message("Faction: " + incidentParms.faction.ToString());
+			//Log.Message("RaidStrategy: " + incidentParms.raidStrategy.ToString());
+			//Log.Message("RaidArrivalMode: " + incidentParms.raidArrivalMode.ToString());
+			//Log.Message("Points: " + incidentParms.points.ToString());
+			//Log.Message("PawnCount: " + incidentParms.pawnCount.ToString());
+			//Log.Message("SpawnCenter: " + incidentParms.spawnCenter.ToString());
+			//Log.Message("Target: " + incidentParms.target.ToString());
 
 
 			Find.Storyteller.incidentQueue.Add(IncidentDefOf.RaidEnemy, Find.TickManager.TicksGame, incidentParms);
