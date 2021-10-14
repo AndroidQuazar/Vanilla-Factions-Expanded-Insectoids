33bce90fbeb53a68a03fb1b81ed76bc82955c314
KylianB <kylian.balde@gmail.com>
2021-10-14 11:34:11 +0200

    Guest royal jelly addiction never drop

--- 1.3/Source/VFEI/RaidStrategyWorkers/RaidStrategyWorker_ImmediateAttackInsect.cs
+++ 1.3/Source/VFEI/RaidStrategyWorkers/RaidStrategyWorker_ImmediateAttackInsect.cs
@@ -3,28 +3,13 @@ using System.Collections.Generic;
 using Verse;
 using Verse.AI.Group;
 
-namespace VFEI.RaidStrategyWorkers
+namespace VFEI
 {
-    public class RaidStrategyWorker_ImmediateAttackInsect : RaidStrategyWorker
+    public class RaidStrategyWorker_ImmediateAttackInsect : RaidStrategyWorker_ImmediateAttack
     {
         public override bool CanUseWith(IncidentParms parms, PawnGroupKindDef groupKind)
         {
-            return base.CanUseWith(parms, groupKind) && parms.faction.def.defName == "VFEI_Insect";
-        }
-
-        public override List<Pawn> SpawnThreats(IncidentParms parms)
-        {
-            Map map = (Map)parms.target;
-            List<Building> ts = map.listerBuildings.allBuildingsColonist;
-            ts.RemoveAll((b) => b.def.defName != "VFEI_SonicInfestationRepeller");
-            int toAdd = Rand.RangeInclusive(0, ts.Count) * 300;
-            parms.points += toAdd;
-            return base.SpawnThreats(parms);
-        }
-
-        protected override LordJob MakeLordJob(IncidentParms parms, Map map, List<Pawn> pawns, int raidSeed)
-        {
-            return new LordJob_AssaultColony(parms.faction, false, false, false, false, false);
+            return parms.faction.def.defName == "VFEI_Insect" && base.CanUseWith(parms, groupKind);
         }
     }
 }
